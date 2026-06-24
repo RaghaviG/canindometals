7455d64f6c70097b0581e9f7319b6c04285d8772
RaghaviG <41799200+RaghaviG@users.noreply.github.com>
2026-06-24 13:34:01 -0400

    pushing new changes
    pushing new changes

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-// function sendMail(){
-//     let parms ={
-//         name : document.getElementById
-//     }
-// }
+function sendMail()
+{
+    let parms ={
+        name : document.getElementById("name").value,
+        message : document.getElementById("message").value,
+        email : document.getElementById("email").value,
+    }
+    emailjs.send("service_dfjs4hq","template_ejst50b",parms).then(alert("Email Sent!"))
+}
